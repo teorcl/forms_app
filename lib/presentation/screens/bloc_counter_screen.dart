0d40a24732b15af6addcb335764eda65001e5d39
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-06-07 22:05:48 -0500

    first commit

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit Counter'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh_outlined),
+          ),
+        ],
+      ),
+      body: Center(child: Text('Counter value xxx')),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () => {},
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () => {},
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+    );
+  }
+}
