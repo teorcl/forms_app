201a35b8496d43108432122be4885ed9fe8e701c
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-06-08 20:35:11 -0500

    feat(presentation): Se crea el counter_cubit

---
+++ lib/presentation/blocs/counter_cubit/counter_state.dart
@@ -0,0 +1,13 @@
+part of 'counter_cubit.dart';
+
+class CounterState {
+  final int counter;
+  final int transactionCount;
+
+  const CounterState({this.counter = 0, this.transactionCount = 0});
+
+  CounterState copyWith({int? counter, int? transactionCount}) => CounterState(
+    counter: counter ?? this.counter,
+    transactionCount: transactionCount ?? this.transactionCount,
+  );
+}
