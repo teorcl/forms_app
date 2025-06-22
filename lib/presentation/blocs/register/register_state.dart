ff6a3ec5ee13eef9d79cbc16d8b7cc462653c3b5
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-06-22 12:46:15 -0500

    feat(register_state.dart): Se cambio el tipo de las propiedades, BREAKING CHANGE: El state ahora requiere que las propiedades sean de un tipo especial

--- lib/presentation/blocs/register/register_state.dart
+++ lib/presentation/blocs/register/register_state.dart
@@ -5,33 +5,38 @@ enum FormStatus { invalid, valid, validating, posting }
 
 class RegisterState extends Equatable {
   final FormStatus formStatus;
-  final String username;
-  final String email;
-  final String password;
+  final Username username;
+  final Email email;
+  final Password password;
+  final bool isValid;
 
   const RegisterState({
     required this.formStatus,
     required this.username,
     required this.email,
     required this.password,
+    required this.isValid,
   });
 
   factory RegisterState.initialState() {
     return const RegisterState(
+      isValid: false,
       formStatus: FormStatus.invalid,
-      username: '',
-      email: '',
-      password: '',
+      username: Username.pure(),
+      email: Email.pure(),
+      password: Password.pure(),
     );
   }
 
   RegisterState copyWith({
     FormStatus? formStatus,
-    String? username,
-    String? email,
-    String? password,
+    Username? username,
+    Email? email,
+    Password? password,
+    bool? isValid,
   }) {
     return RegisterState(
+      isValid: isValid ?? this.isValid,
       formStatus: formStatus ?? this.formStatus,
       username: username ?? this.username,
       email: email ?? this.email,
@@ -40,5 +45,5 @@ class RegisterState extends Equatable {
   }
 
   @override
-  List<Object> get props => [formStatus, username, email, password];
+  List<Object> get props => [formStatus, username, email, password, isValid];
 }
